1564b2ce5b20c263e5778efba13de3f6f8684b2f
Andrei <acenusa58@gmail.com>
2022-10-20 09:59:29 -0900

    make it better

--- handlers/users/post.py
+++ handlers/users/post.py
@@ -77,7 +77,8 @@ async def get_profile():
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
+    # state = await state.get_state()
+    # await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
+    #                      f"\nСодержание сообщения:\n"
+    #                      f"<code>{message}</code>")
+    await message.answer(f"{message.text}")
