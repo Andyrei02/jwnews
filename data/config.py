5c56e49b6bff2523548bdc32ed82227629bfccfa
Andrei <acenusa58@gmail.com>
2022-10-24 00:51:25 -0900

    test project

--- data/config.py
+++ data/config.py
@@ -4,7 +4,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = "5783163714:AAG8l0GKFCzcGyGjw7QBxgVtQGaKwov-OgM"  # Забираем значение типа str
+BOT_TOKEN = ""  # Забираем значение типа str
 ADMINS = ["749333822"]  # Тут у нас будет список из админов
 IP = "192.168.0.1"  # Тоже str, но для айпи адреса хоста
 
